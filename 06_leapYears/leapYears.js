3f5d294cef6de72396d03c54f8020f70b44ca796
tanneragle <tanneragle@gmail.com>
2025-01-24 18:01:06 -0500

    Create function that determines if input year is a leap year.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivisiblebyFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isDivisibleByFourHundred = year % 400 === 0;
 
+    if(isDivisiblebyFour && !isCentury || isDivisibleByFourHundred) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+//Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing)
